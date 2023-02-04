2401632bf27bf1645f0fb06f3707e672fbcb16e5
Sonu Shahuji <sonushahuji4@gmail.com>
2023-02-04 11:45:49 +0530

    creating a server

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express, { Application } from "express";
+import http, { Server } from "http";
+import cors from "cors";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+const app: Application = express();
+
+/** Middlerwaver */
+app.use(express.json({limit: "6mb"}));
+app.use(express.urlencoded({extended: false }));
+app.use(cors());
+
+const server: Server = http.createServer(app);
+server.listen(process.env.SERVER_PORT,() => console.log(`Server running on port : ${process.env.SERVER_PORT}`));
